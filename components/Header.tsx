2fd93bd81f444389ab6b1ee66a8bc56328bbfbeb
hyein89 <bihontai@gmail.com>
2025-12-27 13:02:30 +0700

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <a href="/home">Home</a>
+      <a href="/populer">Populer</a>
+      <a href="/trending">Trending</a>
+    </header>
+  )
+}
