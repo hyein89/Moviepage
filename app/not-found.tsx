e236af3c64c9147e2c39c79a692b2f4cce4bbe0d
hyein89 <bihontai@gmail.com>
2025-12-27 18:06:50 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,25 @@
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Data tidak ditemukan</p>
+    <div id="container">
+      <div className="module">
+        <div className="content right full">
+          <h1 className="heading-archive">Page not found</h1>
+
+          <div className="animation-2 items full">
+            <h2>
+              ERROR <span>404</span>
+            </h2>
+
+            <ul>
+              <li>Please check if the URL is correct.</li>
+              <br />
+              <li>Click the search button to search any movie/show.</li>
+              <br />
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
