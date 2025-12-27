9186411f07d4ebc6b50068c2ba644e8c73ce4daa
hyein89 <bihontai@gmail.com>
2025-12-27 15:05:12 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,76 @@
-
 export const dynamic = 'force-dynamic'
+
 import { tmdbFetch } from '../lib/tmdb'
 
-export default async function Page() {
-  const data = await tmdbFetch('/movie/popular')
+// helper ganti fungsi PHP
+function slugify(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-')
+}
 
-  if (!data) return <h1>TMDB NULL</h1>
+function ratingTwo(vote: number) {
+  return vote ? vote.toFixed(1) : '0.0'
+}
+
+export default async function Page() {
+  const movies = await tmdbFetch('/movie/popular')
+  const tv = await tmdbFetch('/tv/popular')
+
+  if (!movies || !tv) return <h1>TMDB NULL</h1>
 
   return (
-    <ul>
-      {data.results.map((m: any) => (
-        <li key={m.id}>{m.title}</li>
-      ))}
-    </ul>
+    <div id="container">
+      <div className="module">
+        <div className="content right full">
+
+          {/* FEATURED MOVIES */}
+          <div className="animation-2 items full">
+            <h1 className="Featured widget-title">
+              Featured Movies <span><a href="/populer" className="see-all">See all</a></span>
+            </h1>
+
+            {movies.results.map((m: any) => (
+              <article className="item" key={m.id}>
+                <div className="poster">
+                  <img
+                    src={
+                      m.poster_path
+                        ? `https://image.tmdb.org/t/p/w185${m.poster_path}`
+                        : '/img/noposter.png'
+                    }
+                    alt={m.title}
+                  />
+
+                  <div className="rating">
+                    <i className="fa fa-star"></i> {ratingTwo(m.vote_average)}
+                  </div>
+
+                  <div className="mepo"></div>
+
+                  <a href={`/movies/${m.id}/${slugify(m.title)}`}>
+                    <div className="see play3"></div>
+                  </a>
+                </div>
+
+                <div className="data">
+                  <h3>
+                    <a href={`/movies/${m.id}/${slugify(m.title)}`}>
+                      {m.title}
+                    </a>
+                  </h3>
+                  <span>{m.release_date || 'N/A'}</span>
+                </div>
+              </article>
+            ))}
+          </div>
+
+         
+
+        </div>
+      </div>
+    </div>
   )
 }
+
