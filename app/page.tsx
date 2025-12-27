7b73c2fcc5bb2c32d817000cf800e9e4a9811a9e
hyein89 <bihontai@gmail.com>
2025-12-27 13:37:41 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 
-import { redirect } from 'next/navigation'
+export const dynamic = 'force-dynamic'
+import { tmdbFetch } from '../lib/tmdb'
 
-export default function Page() {
-  redirect('/home')
+export default async function Page() {
+  const data = await tmdbFetch('/movie/popular')
+
+  if (!data) return <h1>TMDB NULL</h1>
+
+  return (
+    <ul>
+      {data.results.map((m: any) => (
+        <li key={m.id}>{m.title}</li>
+      ))}
+    </ul>
+  )
 }
