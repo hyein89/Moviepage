169e0a5526f52c9dc9090699093d99ca2ef784fa
hyein89 <bihontai@gmail.com>
2025-12-27 13:08:03 +0700

    Create page.tsx

---
+++ app/trending/page.tsx
@@ -0,0 +1,18 @@
+import { tmdbFetch } from '@/lib/tmdb'
+
+export default async function Page() {
+  const data = await tmdbFetch('/trending/movie/day?language=id-ID')
+
+  return (
+    <div>
+      <h2>Trending</h2>
+      {data?.results.map((m: any) => (
+        <div key={m.id}>
+          <a href={`/movie/${m.id}/${m.title}.html`}>
+            {m.title}
+          </a>
+        </div>
+      ))}
+    </div>
+  )
+}
