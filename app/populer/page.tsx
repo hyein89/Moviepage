3bbff36bee2a41cd8d7741519295a84d6b4086ab
hyein89 <bihontai@gmail.com>
2025-12-27 13:07:13 +0700

    Create page.tsx

---
+++ app/populer/page.tsx
@@ -0,0 +1,18 @@
+import { tmdbFetch } from '@/lib/tmdb'
+
+export default async function Page() {
+  const data = await tmdbFetch('/movie/popular?language=id-ID')
+
+  return (
+    <div>
+      <h2>Populer</h2>
+      {data?.results.map((m: any) => (
+        <div key={m.id}>
+          <a href={`/movie/${m.id}/${m.title}.html`}>
+            {m.title}
+          </a>
+        </div>
+      ))}
+    </div>
+  )
+}
