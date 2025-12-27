09d2387ce12fcb196a489c820e1d19394719a2f2
hyein89 <bihontai@gmail.com>
2025-12-27 15:11:28 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,31 @@
 export default function Footer() {
-  return <footer className="footer">© 2025 Movie Site</footer>
+  return (
+    <footer className="main">
+      <div className="fbox">
+        <div className="fcmpbox">
+          <div className="copy">© 2025 WtsMovies</div>
+
+          <span className="top-page">
+            <a
+              onClick={() =>
+                window.scrollTo({ top: 0, behavior: 'smooth' })
+              }
+            >
+              <i className="fa fa-angle-up"></i>
+            </a>
+          </span>
+
+          <div className="fmenu">
+            <ul id="menu-footer-manu" className="menu">
+              <li><a href="/">Home</a></li>
+              <li><a href="/dmca">DMCA</a></li>
+              <li><a href="/privacy">Privacy Policy</a></li>
+              <li><a href="/terms">Terms and Conditions</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
+
