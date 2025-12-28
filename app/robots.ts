4ef3a3a2287595f477db0a7d450531ea2503d4f9
hyein89 <bihontai@gmail.com>
2025-12-28 20:34:41 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -11,7 +11,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: [
           '/search',
           '/search?',
-          '/*?*', // block semua URL pakai parameter
         ],
       },
     ],
