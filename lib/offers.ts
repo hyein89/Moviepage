583b72182fc39d352259327340c269866d0eef30
hyein89 <bihontai@gmail.com>
2025-12-28 21:13:05 +0700

    Update offers.ts

--- lib/offers.ts
+++ lib/offers.ts
@@ -1,4 +1,4 @@
 export const OFFER_LINKS = {
-  torrent: 'https://offer-domain.com/torrent-offer',
-  subtitle: 'https://offer-domain.com/subtitle-offer',
+  torrent: 'https://signingunwilling.com/kaf4f6xwm?key=e1cded4d4ee6ac60f8ed56c1b7ca2c96',
+  subtitle: 'https://signingunwilling.com/xzrwqmf12?key=baa00e57ed87e9b8d15f8dd1804bd5d8',
 }
