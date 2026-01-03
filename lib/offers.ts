ac92ffa2615827c874efe7c171109eaee08230c7
hyein89 <bihontai@gmail.com>
2026-01-03 16:55:42 +0700

    Update offers.ts

--- lib/offers.ts
+++ lib/offers.ts
@@ -1,4 +1,4 @@
 export const OFFER_LINKS = {
   torrent: 'https://signingunwilling.com/kaf4f6xwm?key=e1cded4d4ee6ac60f8ed56c1b7ca2c96',
-  subtitle: 'https://eminenx-routee.com/?s=4&t1=954&t4=Yasue',
+  subtitle: 'https://imens-poort.com/?s=4&t1=954&t4=Yasue',
 }
