8b797ecbd74b21df7cd504c96b452ba84e153be3
hyein89 <bihontai@gmail.com>
2026-01-04 21:04:16 +0700

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,4 +1,2 @@
-export const SITE = {
-  name: 'WTS Movies',
-  url: 'https://www.xydntvdsg.eu.org',
-}
+export const SITE_NAME = 'WTS Movies'
+export const SITE_URL = 'https://www.xydntvdsg.eu.org'
