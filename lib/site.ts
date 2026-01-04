5ecfd609442049fb4799d4a3c4b91b6ee3df66f5
hyein89 <bihontai@gmail.com>
2026-01-04 21:40:35 +0700

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,4 @@
 export const SITE_NAME = 'WTS Movies'
+export const SITE_DESCRIPTION = 'Watch popular movies, latest releases, trailers, and trending films online. Explore detailed movie information, ratings, and reviews.'
 export const SITE_URL = 'https://www.xydntvdsg.eu.org'
 export const SITE_HOST = 'xydntvdsg.eu.org'
